8e1ddc1deb6360a8e83c130272ba72ce485fb7fc
Fatima Azeemi <fatima647259@gmail.com>
2025-07-05 22:40:29 +0500

    portfolio complete

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,4 +1,57 @@
 from django.contrib import admin
-from .models import PortfolioProject
+from .models import (
+    PortfolioProject,
+    Hero,
+    OverviewSection,
+    RequirementsSection,
+    SolutionsSection,
+    FullWidthImage,
+    ResultSection,
+    TechnologySection,
+    Technology
+)
 
-admin.site.register(PortfolioProject)
+@admin.register(PortfolioProject)
+class PortfolioProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'client_name', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'client_name')
+
+@admin.register(Hero)
+class HeroAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+
+@admin.register(OverviewSection)
+class OverviewAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(RequirementsSection)
+class RequirementsAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(SolutionsSection)
+class SolutionsAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(FullWidthImage)
+class FullWidthImageAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+
+@admin.register(ResultSection)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+class TechnologyInline(admin.TabularInline):
+    model = Technology
+    extra = 1
+
+@admin.register(TechnologySection)
+class TechnologySectionAdmin(admin.ModelAdmin):
+    list_display = ('heading',)
+    inlines = [TechnologyInline]
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'section')
+    list_filter = ('section',)
+    search_fields = ('name',)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
-from .models import PortfolioProject
+from django.shortcuts import render, get_object_or_404
+from .models import (
+    PortfolioProject,
+    Hero,
+    OverviewSection,
+    RequirementsSection,
+    SolutionsSection,
+    ResultSection,
+    FullWidthImage,
+    TechnologySection
+)
 
 def portfolio_list(request):
     projects = PortfolioProject.objects.all()
     return render(request, 'portfolio/portfolio.html', {'projects': projects})
+
+
+def portfolio_detail(request, slug):
+    project = get_object_or_404(PortfolioProject, slug=slug)
+
+    hero = Hero.objects.first()
+    overview = OverviewSection.objects.first()
+    requirements = RequirementsSection.objects.first()
+    solutions = SolutionsSection.objects.first()
+    result = ResultSection.objects.first()
+    full_image = FullWidthImage.objects.first()
+    tech_section = TechnologySection.objects.prefetch_related('technologies').first()
+
+    context = {
+        'project': project,
+        'hero': hero,
+        'overview': overview,
+        'requirements': requirements,
+        'solutions': solutions,
+        'result': result,
+        'full_image': full_image,
+        'tech_section': tech_section,
+    }
+
+    return render(request, 'portfolio/portfolio_detail.html', context)
