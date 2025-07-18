23e9df1db428afc876ca2de9ea3498f6c231fa04
Fatima Azeemi <fatima647259@gmail.com>
2025-07-18 16:38:34 +0500

    changing jo krni thi done

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import CompanyInfo, SocialLink, SiteSetting
+from .models import ContactMessage
 
 admin.site.register(CompanyInfo)
 admin.site.register(SocialLink)
 admin.site.register(SiteSetting)
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'company_name', 'created_at')
+    search_fields = ('name', 'email', 'company_name')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
